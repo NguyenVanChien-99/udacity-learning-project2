14f70276772f78e0dbd3ce100ce16fbef6bd0478
Chien Nguyen <nguyenvanchien309318@gmail.com>
2022-06-08 21:59:50 +0700

    implement endpoint filter image

--- src/server.ts
+++ src/server.ts
@@ -13,15 +13,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+    //CORS Should be restricted
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      next();
+    });
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
   //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  //    1. validate the image_url query ==>ok
+  //    2. call filterImageFromURL(image_url) to filter the image ==>ok
+  //    3. send the resulting file in the response ==>ok
+  //    4. deletes any files on the server on finish of the response 
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
@@ -33,8 +39,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/filteredimage", async ( req, res ) => {
+    const imageUrl:string = req.query.image_url;
+    console.log(imageUrl);
+    if(!imageUrl||imageUrl==""){
+      return res.status(404).send({msg:`image_url is required`})
+    }
+    const savedLocalFile = await filterImageFromURL(imageUrl)
+    return res.status(200).sendFile(savedLocalFile);
   } );
   
 
